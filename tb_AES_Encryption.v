6f4d8f62de059b0d191c1af80e2050f41c908fe3
Abdallah Ahmed <abdallah.ahmed581@gmail.com>
2023-12-31 22:31:46 +0200

    testbench_test

--- tb_AES_Encryption.v
+++ tb_AES_Encryption.v
@@ -12,14 +12,17 @@ module tb_AES_Encryption;
 
     // Display the output after each round
     initial begin
-        plain_text = 128'h3243f6a8885a308d313198a2e0370734;
-        key = 128'h2b7e151628aed2a6abf7158809cf4f3c;
+        //plain_text = 128'h3243f6a8885a308d313198a2e0370734;
+        //key = 128'h2b7e151628aed2a6abf7158809cf4f3c;
+	plain_text = 128'h00112233445566778899aabbccddeeff;
+	key = 128'h000102030405060708090a0b0c0d0e0f;
         #10;        
         $display("Input plain text: %h", plain_text);
         $display("Input key: %h", key);
         // check if the output is 3925841d02dc09fbdc118597196a0b32
         // if correct then display test passed
-        if (cipher == 128'h3925841d02dc09fbdc118597196a0b32)
+        //if (cipher == 128'h3925841d02dc09fbdc118597196a0b32)
+	if (cipher == 128'h69c4e0d86a7b0430d8cdb78070b4c55a)
             $display("Test passed");
         else
             $display("Test failed");
